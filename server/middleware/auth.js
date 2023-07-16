c60d4e9ffddc21bdc318f12b1757c2f93b92e9f6
Deepak Singh <singhdeepak5219@gmail.com>
2023-07-16 14:57:17 +0530

    adding sever API

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    console.log(req.headers.authorization);
+    if(req.headers.authorization) { const token = req.headers.authorization.split(' ')[1]; 
+    // const token = req.headers.authorization.split(" ")[1];
+    let decodedData;
+    if (token) {
+      decodedData = jwt.verify(token, "sEcReT");
+      req.userId = decodedData?.id;
+    }
+  }
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
---
+++ server/routes/studentRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  studentLogin,
+  updatedPassword,
+  updateStudent,
+  testResult,
+  attendance,
+  
+} from "../controller/studentController.js";
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post("/login", studentLogin);
+router.post("/updatepassword", auth, updatedPassword);
+router.post("/updateprofile", auth, updateStudent);
+router.post("/testresult", auth, testResult);
+router.post("/attendance", auth, attendance);
+// router.post("/getsubject", auth, getSubject);
+
+export default router;
